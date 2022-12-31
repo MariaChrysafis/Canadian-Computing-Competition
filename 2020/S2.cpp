0981a0c15b48d5724910f1a6ef8d6c00f08f2c23
Olympia <maria.chrysafis.junior@gmail.com>
2022-12-31 12:48:57 -0800

    Update S2.cpp

--- 2020/S2.cpp
+++ 2020/S2.cpp
@@ -16,73 +16,46 @@
 #include <ctime>
 
 using namespace std;
-struct graph{
-    const int inf = 1e8;
-    vector<vector<pair<int,int>>> adj; //{-weight, node}
-    vector<int> dijk(int src){
-        vector<int> dist;
-        vector<bool> mark;
-        dist.resize(adj.size()), mark.resize(adj.size());
-        for(int i = 0; i < adj.size(); i++){
-            dist[i] = inf;
-            mark[i] = false;
-        }
-        dist[src] = 0;
-        priority_queue<pair<int,int>> pq;
-        pq.push({0,src});
-        while(!pq.empty()){
-            int u = pq.top().second; //node
-            pq.pop();
-            if(mark[u]){
-                continue;
-            }
-            mark[u] = true;
-            for(auto p: adj[u]){
-                int v = p.second;
-                int w = p.first;
-                if(dist[v] > dist[u] + w){
-                    dist[v] = dist[u] + w;
-                    pq.push({-dist[v], v});
-                }
-            }
-        }
-        return dist;
-    }
-};
-int gcd (int x, int y) {
-    return (min(x, y) == 0 ? max(x, y) : gcd(max(x, y) % min(x, y), min(x, y)));
-}
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int N, M;
     cin >> N >> M;
+    vector<int> adj[N * M];
     int arr[N + 1][M + 1];
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= M; j++) {
-            cin >> arr[i][j];
-        }
-    }
-    vector<pair<int,int>> fact[N * M + 1];
-    for (int i = 1; i <= N; i++) {
-        for (int f = i; f <= N * M; f += i) {
-            if (f % i == 0 && f/i <= M && i <= N) {
-                fact[f].emplace_back(i, f/i);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cin >> arr[i + 1][j + 1];
+            if (arr[i + 1][j + 1] <= N * M) {
+                adj[arr[i + 1][j + 1] - 1].push_back(M * i + j + N * M); //index to product of indices
             }
         }
     }
-    graph gr;
-    gr.adj.resize(N * M);
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= M; j++) {
-            if (arr[i][j] > N * M) {
-                continue;
+    bool vis[2 * N * M];
+    for (int i = 0; i < 2 * N * M; i++) {
+        vis[i] = false;
+    }
+    queue<int> q;
+    q.push(2 * N * M - 1);
+    vis[2 * N * M - 1] = true;
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        if (x >= N * M) {
+            x -= N * M;
+            int new_guy = (x/M + 1) * (x % M + 1) - 1;
+            if (!vis[new_guy]) {
+                q.push(new_guy);
+                vis[new_guy] = true;
             }
-            for (auto& p: fact[arr[i][j]]) {
-                gr.adj[M * i + j - M - 1].emplace_back(1, M * p.first + p.second - M - 1);
+        } else {
+            for (int i: adj[x]) {
+                if (!vis[i]) {
+                    q.push(i);
+                    vis[i] = true;
+                }
             }
         }
     }
-    auto dum = gr.dijk(0);
-    cout << ((dum[N * M - 1] == gr.inf) ? "no" : "yes") << '\n';
+    cout << (vis[N * M] ? "yes" : "no");
 }
